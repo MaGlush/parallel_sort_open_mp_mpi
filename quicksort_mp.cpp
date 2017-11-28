2d3b1a6c9e2e8a41ba9af19c5a5d2736a9846c2e
WorrisomeAsu <glushchenkomaya@gmail.com>
2017-11-28 20:02:33 +0300

    init

--- quicksort_mp.cpp
+++ quicksort_mp.cpp
@@ -6,10 +6,14 @@
 #include <iostream>
 
 #define SIZE 100000
-double matrix[SIZE][SIZE]
+double matrix[SIZE][SIZE];
 
-double *Initialization(double *matrix) {
-	
+double *Initialization(double **matrix) {
+    srand(time(NULL));
+	for (int i = 0; i < SIZE; i++)
+		for (int j = 0; j < 0; j++) {
+			matrix[i][j] = rand() % 100;
+		}
 }
 
 int main() 
